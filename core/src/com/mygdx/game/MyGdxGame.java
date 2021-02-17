c98c333eb19d7a656d18260e93f0e1d52769f21a
Renan Castro <renancas_ribeiro@hotmail.com>
2021-02-17 03:25:40 -0300

    Aula 36
    Assets

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,21 +1,49 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
-public class MyGdxGame extends Game {
+public class MyGdxGame extends ApplicationAdapter {
+
+	private AssetManager assetManager;
+
+	private SpriteBatch batch;
+	private Texture felpudo1;
 
 	@Override
 	public void create () {
-		setScreen(new MainScreen(this));
+		assetManager = new AssetManager();
+
+		for(int i = 1 ; i <=7 ; i++){
+			assetManager.load("Felpudo/felpudo"+i+".png", Texture.class);
+		}
+
+		assetManager.finishLoading();
+
+		boolean loaded = assetManager.isLoaded("Felpudo/felpudo1.png");
+		 Gdx.app.log("Log", String.valueOf(loaded));
+
+		 loaded = assetManager.isLoaded("Felpudo/felpudo9.png");
+		 Gdx.app.log("Log", String.valueOf(loaded));
+		 batch = new SpriteBatch();
+		felpudo1 = assetManager.get("Felpudo/felpudo1.png", Texture.class );
+	}
+
+	@Override
+	public void render() {
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(felpudo1, 0, 0, 300, 300 );
+		batch.end();
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
 	}
 }
