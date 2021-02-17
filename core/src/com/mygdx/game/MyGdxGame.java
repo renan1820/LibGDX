eafd2eebb11363a883bc9145a5a38ec3a83be7d9
Renan Castro <renancas_ribeiro@hotmail.com>
2021-02-17 02:59:19 -0300

    Aula 34
    Put Sounds

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,6 +3,7 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -12,31 +13,26 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class MyGdxGame extends ApplicationAdapter {
 
-	private ShapeRenderer shapeRenderer;
+	private Sound sound;
+	private Long id;
 	@Override
 	public void create () {
-		shapeRenderer = new ShapeRenderer();
-		shapeRenderer.setAutoShapeType(true);
+		sound = Gdx.audio.newSound(Gdx.files.internal("som1.mp3"));
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		shapeRenderer.begin();
-		shapeRenderer.setColor(Color.RED);
-//		shapeRenderer.setColor(1,0,1,1);
-		shapeRenderer.set(ShapeRenderer.ShapeType.Filled);
-		shapeRenderer.rect(100,100, 300, 400);
-		shapeRenderer.rect(500, 200, 150, 150  );
 
-		shapeRenderer.setColor(Color.GOLD);
-		shapeRenderer.circle(500,500, 200);
-		shapeRenderer.end();
+		sound.play();
+		id = sound.loop();
+
+		sound.stop(id);
 	}
 
 	@Override
 	public void dispose() {
-		super.dispose();
+		sound.dispose();
 	}
 }
