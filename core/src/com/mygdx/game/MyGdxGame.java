9a5ea2872c7c3c845ac44c7a8a78b75462f8cd41
Renan Castro <renancas_ribeiro@hotmail.com>
2021-02-17 02:52:11 -0300

    Aula 33
    Create Shapes

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,85 +3,40 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
-public class MyGdxGame extends ApplicationAdapter  implements InputProcessor {
-	SpriteBatch batch;
-	Texture img;
+public class MyGdxGame extends ApplicationAdapter {
 
-	Sprite personagem;
+	private ShapeRenderer shapeRenderer;
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		personagem = new Sprite(img);
-		personagem.setPosition(500,500);
-		personagem.setSize(500,500);
-		personagem.setRotation(120);
-		personagem.setFlip(false,true);
-		Gdx.input.setInputProcessor(this);
+		shapeRenderer = new ShapeRenderer();
+		shapeRenderer.setAutoShapeType(true);
 	}
 
 	@Override
 	public void render () {
-//		if (Gdx.input.isTouched()){
-//			 Gdx.app.log("Log", "tocou na tela: X: " + Gdx.input.getX() + " Y: "+ + Gdx.input.getY());
-//		}
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		personagem.draw(batch);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		shapeRenderer.begin();
+		shapeRenderer.setColor(Color.RED);
+//		shapeRenderer.setColor(1,0,1,1);
+		shapeRenderer.set(ShapeRenderer.ShapeType.Filled);
+		shapeRenderer.rect(100,100, 300, 400);
+		shapeRenderer.rect(500, 200, 150, 150  );
+
+		shapeRenderer.setColor(Color.GOLD);
+		shapeRenderer.circle(500,500, 200);
+		shapeRenderer.end();
 	}
 
 	@Override
-	public boolean keyDown(int keycode) {
-		return false;
-	}
-
-	@Override
-	public boolean keyUp(int keycode) {
-		return false;
-	}
-
-	@Override
-	public boolean keyTyped(char character) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		personagem.setPosition(screenX, Gdx.graphics.getHeight() - screenY);
-		return false;
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(float amountX, float amountY) {
-		return false;
+	public void dispose() {
+		super.dispose();
 	}
 }
